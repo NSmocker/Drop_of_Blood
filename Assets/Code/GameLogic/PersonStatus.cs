a3e842f759d69d92b7cb408707cedc350326ba19
NSmocker <sinchik97@gmail.com>
2019-02-09 13:29:49 +0200

    Revert "Revert "Актуализация версии""
    This reverts commit 8820165cb4e2e7c00ecd8f0eb53a2ae102aa9da3.

---
+++ Assets/Code/GameLogic/PersonStatus.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public partial class PersonStatus : MonoBehaviour
+{
+    public int Hp_current,HP_max, MP;
+    public int AutoAttack_Damage;
+    public gameCategory category;
+    public gameSide side;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Test/TestSwipe.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+public class TestSwipe : MonoBehaviour {
+	
+	public GameObject toGenerate;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		Debug.Log (CrossPlatformInputManager.GetAxis ("Melle"));
+		if (CrossPlatformInputManager.GetAxis ("Melle")>0)
+		{
+
+			
+			Destroy(Instantiate (toGenerate, transform.position, transform.rotation,transform),30f);
+		}
+
+	}
+}
