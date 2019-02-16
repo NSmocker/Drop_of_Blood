f0b8629475bbea0a434a3af3249659894fb9003f
NSmocker <sinchik97@gmail.com>
2019-02-16 22:46:38 +0200

    Крип сил света, моделька без рига.

--- Assets/Code/GameLogic/CreepAI/CreepMelleController.cs
+++ Assets/Code/GameLogic/CreepAI/CreepMelleController.cs
@@ -3,40 +3,37 @@ using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 
-public class DemonCreepMelleController : MonoBehaviour
+public class CreepMelleController : MonoBehaviour
 {
+
+
+    Rigidbody rb;
     PersonStatus status;
+    int move_speed;
+
     public GameObject target;
-    public Transform []WeyPoints;
-    public Rigidbody rb;
-    public float move_speed; // скорость движения крипа.
-
-
+    public Transform[] WeyPoints;
     public int current_weypoint=0;
     public float distance_to_point;
-
     public Collider[] objects;
     public GameObject[] Persons;
+
+
+
 
     public void ScanEnviro()
     {
         objects = Physics.OverlapSphere(transform.position, 10f);
-
-
         foreach(Collider x in objects)
         {
             PersonStatus st = x.gameObject.GetComponent<PersonStatus>();
             if(st!=null && !Persons.Contains(x.gameObject))Persons = Persons.Concat(new GameObject[]{ x.gameObject}).ToArray();
             st = null;
         }
-
         foreach (GameObject x in Persons)
         {
          if (!objects.Contains(x.GetComponent<Collider>())) Persons = Persons.Where(val => val != x).ToArray();
         }
-
-
-
     }
 
 
@@ -60,6 +57,9 @@ public class DemonCreepMelleController : MonoBehaviour
         // первым делом, поворачиваем морду к вражине (ну или к точке передвижения)
 
         RotateToTarget(WeyPoints[current_weypoint]);
+        move_speed = status.move_speed;// Смотрим какая у нас скорость движения.
+
+
         if (current_weypoint != WeyPoints.Length) // пока мы не на последней точке
         {
             if (distance_to_point <= 1) //  Если дистанция Метр или меньше  
--- Assets/Code/GameLogic/PersonStatus.cs
+++ Assets/Code/GameLogic/PersonStatus.cs
@@ -4,8 +4,11 @@ using UnityEngine;
 
 public partial class PersonStatus : MonoBehaviour
 {
-    public int Hp_current,HP_max, MP;
-    public int AutoAttack_Damage;
+    public int hp_current,hp_max,mp_max, mp_current;// Показатели ХП и маны.
+    public int damage;//Дамаг
+    public int move_speed;// Скорость бега. 
+
+
     public gameCategory category;
     public gameSide side;
 
